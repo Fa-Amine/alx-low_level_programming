535a8dafedfa26b69f7b1e3cc69fc01e4939b8f7
maf <qfZF@GMAIL.COM>
2023-03-18 11:18:26 -0700

    m

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,15 @@
 */
 int main(void)
 {
-        int i;
+	int i;
 	int j;
 for (i = 97; i <= 122; i++)
 {
-        putchar(i);
+	putchar(i);
 }
 for (j = 65; j <= 90; j++)
 {
-        putchar(j);
+	putchar(j);
 }
 putchar('\n');
 return (0);
