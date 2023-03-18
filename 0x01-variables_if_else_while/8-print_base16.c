909d819a975140aae63f6938641cb3243d6171fe
maf <qfZF@GMAIL.COM>
2023-03-18 13:21:18 -0700

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - HEX
+*
+* Description: using the main function
+* this program prints "the number n is positive, zero, or negative
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+for (i = 48 ; i <= 57 ; i++)
+{
+	putchar(i);
+
+}
+for (j = 97 ; j <= 102 ; j++)
+{
+	putchar(j);
+}
+putchar('\n');
+return (0);
+}
