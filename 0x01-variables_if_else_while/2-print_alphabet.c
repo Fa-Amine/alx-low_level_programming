d10f825c37e6c3c45b78ddc654c2658f63d32ef7
maf <qfZF@GMAIL.COM>
2023-03-18 10:41:17 -0700

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* main - print the alphabet
+*
+* Description: using the main function
+* this program prints "the number n is positive, zero, or negative
+* Return: 0
+*/
 int main(void)
 {
-
+	int i;
+for (i = 97; i <= 122; i++)
+{
+        putchar(i); // Print out the character
+}
+}
