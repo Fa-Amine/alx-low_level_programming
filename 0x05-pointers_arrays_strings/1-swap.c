c60e358758d265aff711c3de92ef5ccf71b11aa3
maf <qfZF@GMAIL.COM>
2023-03-27 08:46:49 -0700

    m

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
-/*
- * nanan
- *
+/**
+ * swap_int - switch the values of two integers
+ * @a: the first integer to switch
+ * @b: the second integer to swap
  */
 void swap_int(int *a, int *b)
 {
-	*a = b;
-	*b = a;
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
 }
