ba5ff3e8679fc32d2bc3e6c021f18d7091510586
maf <qfZF@GMAIL.COM>
2023-03-27 09:41:35 -0700

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - reverses string and adds line
+ *
+ * @s : pointer does stuff
+ */
+void print_rev(char *s)
+{
+	int i;
+	int len = strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
