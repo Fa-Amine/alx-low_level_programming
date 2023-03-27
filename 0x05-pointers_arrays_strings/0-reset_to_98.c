3d01fa1b93ca2c103f2d395afc9ba36613c3cb03
maf <qfZF@GMAIL.COM>
2023-03-27 08:43:33 -0700

    m

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update the value of n by pointing to 98
- * @n: pointer to an int to update
+ * reset_to_98 - update the value of p by pointing to 98
+ * @p: pointer to an int to update
  */
 void reset_to_98(int *p)
 {
