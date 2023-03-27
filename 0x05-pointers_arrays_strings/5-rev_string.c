b6b94d539e66ab15d011ccd6cf8d6f535ee099f3
maf <qfZF@GMAIL.COM>
2023-03-27 10:42:43 -0700

    m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,11 +13,13 @@ void rev_string(char *s)
 	int len;
 
 	len = strlen(s);
-	for (i = len ; i > len / 2 ; i--)
+	for (i = 0 ; i <= len / 2 ; i++)
 	{
 		char tmp = *p;
 		*p = *s;
 		*s = tmp;
+		p--;
+		s++;
 		_putchar(s[i]);
 	}
 }
