574997d20f4adfe508f6b81db00e5585d34fc27b
maf <qfZF@GMAIL.COM>
2023-03-29 14:26:16 -0700

    m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int sz1 = strlen(s1);
-	int sz2 = strlen(s2);
-	int diff;
+	int i;
 
-	if (sz1 < sz2)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		diff = -15;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	else if (sz1 > sz2)
-	{
-		diff = 15;
-	}
-	else  if (sz1 == sz2)
-	{
-		diff = 0;
-	}
-return (diff);
+	return (0);
+
 }
